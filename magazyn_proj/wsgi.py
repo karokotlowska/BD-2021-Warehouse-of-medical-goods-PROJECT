333e7011bd84b0f82f3dd31c75f26d49ee6e10d5
kotlowska@student.agh.edu.pl <kotlowska@student.agh.edu.pl>
2022-01-09 20:54:20 +0100

    Initial commit

--- magazyn_proj/wsgi.py
+++ magazyn_proj/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'magazyn_med_proj.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'magazyn_proj.settings')
 
 application = get_wsgi_application()
