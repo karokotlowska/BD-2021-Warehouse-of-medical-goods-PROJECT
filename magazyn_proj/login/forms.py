bcf80e618df725b4c93a43e9c64340ed5346d827
karokotlowska <62810585+karokotlowska@users.noreply.github.com>
2022-03-13 18:51:09 +0100

    Delete magazyn_proj directory

--- magazyn_proj/login/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-
-
-class loginForm(forms.Form):
-    login = forms.CharField(label = 'login', max_length=100)
-    password = forms.CharField(label = 'haslo', max_length=100, widget=forms.PasswordInput())
--- magazyn_proj/storehouse/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class StorehouseConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'storehouse'
